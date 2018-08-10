556b81fb3fe05af5f12bc345ea9614d1dcc1b754
Shoster <hset2000@gmail.com>
2018-08-11 01:52:01 +0300

    Added Comments

--- app/controller/admincontroller.php
+++ app/controller/admincontroller.php
@@ -6,9 +6,11 @@ class AdminController extends Controller
 
     public function save()
     {
+        // if admin has not entered a new password, remove it from $_POST, so password will not be rewritten to null.
         if ($_POST['password'] == null) {
             unset($_POST['password']);
         }
+        // parent deals with the rest ->create new or update.
         parent::saveEntity();
         header('Location:index.php?route=admin');
         die();
