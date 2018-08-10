ba518bc67cd0e1f5f3ff5dd0d96b575b4cf156b7
Shoster <hset2000@gmail.com>
2018-08-11 01:48:46 +0300

    Added Comments +changed function name

--- app/view/schoolview.php
+++ app/view/schoolview.php
@@ -2,6 +2,11 @@
 require_once 'app/view/view.php';
 class SchoolView extends View
 {
+    //$courses = array of all courses
+    //$students = array of all students
+    //$connected_type_info = array of connected entities to the selcted entity.
+    //$connected_type_name = string -> conncted entities type name. (student or course);
+    //$type_columns_names = entity fields names (keys).
     protected $courses;
     protected $students;
     protected $connected_type_info;
@@ -12,9 +17,9 @@ class SchoolView extends View
     {
         $this->courses = $this->model->getCourses();
         $this->students = $this->model->getStudents();
-        $this->connected_type_info = $this->model->getMainContainerConnectedTypeInfo();
-        $this->connected_type_name = $this->model->getConnectedTypeName();
-        $this->type_columns_names = $this->model->getTypeColumnsNames();
+        $this->connected_type_info = $this->model->getConnectedEntityInfo();
+        $this->connected_type_name = $this->model->getConnectedEntityName();
+        $this->type_columns_names = $this->model->getEntityFieldsNames();
         parent::output();
     }
 }
